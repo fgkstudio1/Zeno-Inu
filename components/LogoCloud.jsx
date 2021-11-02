92b7efc65866e7507354a57982659b55a7d2bcdd
Ken Kauksi <kensservices@gmail.com>
2021-11-03 01:25:23 +0200

    add lp lock info

--- components/LogoCloud.jsx
+++ components/LogoCloud.jsx
@@ -5,27 +5,30 @@ export default function LogoCloud() {
         <div className="text-center uppercase text-lightGrey font-medium">Find us on</div>
         <div className="flex flex-col lg:flex-row gap-8 mt-4 justify-between">
           <a href="https://coinmarketcap.com/currencies/zeno-inu/" target="_blank" rel="noopener" className="flex justify-center" title="SOON!">
-            <img src="/coinmarketcap.png" className="h-10 min-w-[230px]" alt="CoinMarketCap" />
+            <img src="/coinmarketcap.png" className="h-9 min-w-[207px]" alt="CoinMarketCap" />
           </a>
           <a href="https://www.coingecko.com/en/coins/zeno-inu" target="_blank" rel="noopener" className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <img src="/coingecko.png" className="h-10 min-w-[182px]" alt="CoinGecko" />
+            <img src="/coingecko.png" className="h-9 min-w-[164px]" alt="CoinGecko" />
           </a>
           <a href="https://app.uniswap.org/#/swap?outputCurrency=0x7D2ee0a3856cfFD58fB0963C0E24BF1277a0be0e&use=V2" target="_blank" rel="noopener" className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <img src="/uniswap.png" className="h-10 min-w-[190px]" alt="UniSwap" />
+            <img src="/uniswap.png" className="h-9 min-w-[171px]" alt="UniSwap" />
           </a>
           <a href="https://www.dextools.io/app/ether/pair-explorer/0x216a09bdf716b20b7c16bb164d9e3cc69b3c79fb" target="_blank" rel="noopener" className="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
             <img
               src="/dextools.png"
               alt="DexTools"
-              className="h-10 min-w-[177px]"
+              className="h-9 min-w-[159px]"
             />
           </a>
           <a href="https://etherscan.io/token/0x7d2ee0a3856cffd58fb0963c0e24bf1277a0be0e" target="_blank" rel="noopener" className="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
             <img
               src="/etherscan.png"
               alt="Etherscan"
-              className="h-10 min-w-[177px]"
+              className="h-9 min-w-[159px]"
             />
+          </a>
+          <a href="https://www.team.finance/view-coin/0x7D2ee0a3856cfFD58fB0963C0E24BF1277a0be0e?name=Zeno%20Inu&symbol=ZENO" target="_blank" rel="noopener" className="flex justify-center" title="SOON!">
+            <img src="/teamfinance.png" className="h-9 min-w-[97px]" alt="CoinMarketCap" />
           </a>
         </div>
       </div>
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
       <Html lang="en-US">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
           <script
             async
