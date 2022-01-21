445c7e42560073139236edae7bcd2be56dae856a
fgkstudio1 <75630430+fgkstudio1@users.noreply.github.com>
2022-01-22 02:39:57 +0700

    s

--- components/Join.jsx
+++ components/Join.jsx
@@ -5,13 +5,13 @@ const Join = () => {
         <h3 className="text-2xl lg:text-5xl">Join our community<br/> and rule the meme<br /> tokens with us!</h3>
         <div className="flex flex-col lg:flex-row mt-10 gap-6">
           <a
-            href="https://t.me/zenoinu" target="_blank" rel="noopener"
+            href="https://t.me/unipadportal" target="_blank" rel="noopener"
             className="inline-flex items-center px-4 py-2 text-base text-black font-bold bg-teal hover:bg-gray-700"
           >
             Join our telegram
           </a>
           <a
-            href="https://twitter.com/ZenoInu" target="_blank" rel="noopener"
+            href="https://twitter.com/unipadnetwork" target="_blank" rel="noopener"
             className="inline-flex items-center px-4 py-2 text-base text-white font-bold bg-lighterGrey hover:bg-gray-700"
           >
             Follow us on twitter
--- components/LogoCloud.jsx
+++ components/LogoCloud.jsx
@@ -10,25 +10,25 @@ export default function LogoCloud() {
           <a href="https://www.coingecko.com/en/coins/zeno-inu" target="_blank" rel="noopener" className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
             <img src="/coingecko.png" className="h-9 min-w-[164px]" alt="CoinGecko" />
           </a>
-          <a href="https://app.uniswap.org/#/swap?outputCurrency=0x7D2ee0a3856cfFD58fB0963C0E24BF1277a0be0e&use=V2" target="_blank" rel="noopener" className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <img src="/uniswap.png" className="h-9 min-w-[171px]" alt="UniSwap" />
+          <a href="#" target="_blank" rel="noopener" className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+            <img src="/pcs.png" className="h-9 min-w-[171px]" alt="PancakeSwap" />
           </a>
-          <a href="https://www.dextools.io/app/ether/pair-explorer/0x216a09bdf716b20b7c16bb164d9e3cc69b3c79fb" target="_blank" rel="noopener" className="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
+          <a href="#" target="_blank" rel="noopener" className="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
             <img
               src="/dextools.png"
               alt="DexTools"
               className="h-9 min-w-[159px]"
             />
           </a>
-          <a href="https://etherscan.io/token/0x7d2ee0a3856cffd58fb0963c0e24bf1277a0be0e" target="_blank" rel="noopener" className="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
+          <a href="#" target="_blank" rel="noopener" className="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
             <img
-              src="/etherscan.png"
-              alt="Etherscan"
+              src="/bsc.png"
+              alt="BSCscan"
               className="h-9 min-w-[159px]"
             />
           </a>
-          <a href="https://www.team.finance/view-coin/0x7D2ee0a3856cfFD58fB0963C0E24BF1277a0be0e?name=Zeno%20Inu&symbol=ZENO" target="_blank" rel="noopener" className="flex justify-center" title="SOON!">
-            <img src="/teamfinance.png" className="h-9 min-w-[97px]" alt="CoinMarketCap" />
+          <a href="#" target="_blank" rel="noopener" className="flex justify-center" title="SOON!">
+            <img src="/teamfinance.png" className="h-9 min-w-[97px]" alt="Pinksale" />
           </a>
         </div>
       </div>
